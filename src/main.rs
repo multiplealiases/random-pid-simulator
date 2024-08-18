aea19303c2a801c5d402cec910c57ae8e866518d
multiplealiases <conflictvegetable@protonmail.com>
2024-08-18 09:52:02 +0800

    Revert "non-BitVec impl much faster"
    This reverts commit f2d11aaa6dfd2563722dd5603523400eb74ec1d1.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use rand::prelude::*;
+use bitvec::prelude::*;
+use bitvec::bitvec;
 // PIDs 0 and 1 can't be reassigned.
 const PID_SPACE: usize = (1 << 24) - 2;
 const SIMULATIONS: usize = 1000;
@@ -8,7 +10,7 @@ fn main() {
     eprint!("Simulation ");
     for s in 0..SIMULATIONS {
         eprint!("{s} ");
-        let mut pids = vec![false; PID_SPACE];
+        let mut pids = bitvec![0; PID_SPACE];
         for i in 0..PID_SPACE {
             let (_, tries) = tryassign(&mut pids);
             tries_existing_process[i] += tries as f64;
@@ -19,7 +21,7 @@ fn main() {
     println!("{tries_existing_process_avg:?}");
 }
 
-fn tryassign(pids: &mut [bool]) -> (usize, usize) {
+fn tryassign(pids: &mut BitVec) -> (usize, usize) {
     let mut rng = thread_rng();
     let mut success = false;
     let mut tries = 0;
@@ -28,7 +30,7 @@ fn tryassign(pids: &mut [bool]) -> (usize, usize) {
         tries += 1;
         candidate = rng.gen_range(0..PID_SPACE);
         if !pids[candidate] {
-            pids[candidate] = true;
+            pids.set(candidate, true);
             success = true;
         }
     }
